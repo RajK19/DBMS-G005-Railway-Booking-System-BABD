0d43e838ab199693efa2f5c0e37dedcb0edfa322
Raj Kansal <rajkansal2001@gmail.com>
2023-11-07 14:58:01 +0530

    Add files via upload

---
+++ DBMS_G005_DQL_Raj_RailwaysBookingSystem.sql
@@ -0,0 +1,83 @@
+select * from ticket;
+
+select * from user;
+
+select * from class;
+
+select * from payments;
+
+select * from routes;
+
+SELECT t.Passenger_name,tr.TrainName
+FROM Ticket t
+JOIN Train tr ON t.Train_No = tr.TrainNumber;
+
+SELECT SUM(amount) AS TotalAmountSpent
+FROM Payments;
+
+SELECT Passenger_name,Class
+FROM Ticket
+WHERE Class = '2A';
+
+SELECT TrainNumber, Via
+FROM Routes
+WHERE TrainNumber IN (SELECT TrainNumber FROM Train WHERE Source = 'Jammu Tawi [JAT]');
+
+
+SELECT passenger_name, bank_name
+FROM Payments
+WHERE amount > 2000;
+
+SELECT Class_type, AVG(Fare_Rs) AS TotalFare
+FROM Class
+GROUP BY Class_type;
+
+
+SELECT PNR_NO, Passenger_name
+FROM Ticket
+WHERE Source = 'Howrah Junction [HWH]' AND Destination = 'New Delhi [NDLS]';
+
+
+SELECT TrainNumber, Source, Destination
+FROM Train
+WHERE DistanceKms > 1500;
+
+SELECT TrainName, Scheduled_Departure
+FROM Train
+WHERE Source = 'Howrah Junction [HWH]' AND DistanceKms > 1000;
+
+SELECT PNR_NO, Train_No, Train_Name, Passenger_name
+From Ticket
+Where Source = 'Pune Junction [PUNE]';
+
+
+
+SELECT *
+FROM Routes
+WHERE TrainNumber IN (
+SELECT TrainNumber
+FROM Train
+WHERE Source = 'Pune Junction [PUNE]'
+)
+LIMIT 2;
+
+SELECT AVG(Fare_Rs) AS AverageFare
+FROM Class
+WHERE Class_type = '3A';
+
+SELECT *
+FROM Payments
+WHERE bank_name LIKE '%ICICI%';
+
+
+SELECT Train_No
+FROM Class
+WHERE Fare_Rs = (SELECT MAX(Fare_Rs) FROM Class WHERE Class_type = '1A');
+
+
+SELECT Passenger_name
+FROM Ticket
+WHERE Seat_no < 30;
+
+
+
---
+++ SQL railway table.sql
@@ -0,0 +1,363 @@
+drop database if exists railway;
+
+create database railway;
+use railway;
+
+drop table if exists User;
+
+create table User(
+Username varchar(255) Primary Key NOT NULL,
+Name varchar(255) NOT NULL,
+Address varchar(255) NOT NULL,
+DOB varchar(255) NOT NULL,
+Age int(10) NOT NULL,
+Mobile_no INT NOT NULL,
+Gender varchar(255) NOT NULL
+);
+
+
+INSERT INTO User Values
+('ppatel_0812','Priya Patel','Ahmed Mamuji Street Liluah Howrah West Bengal 711204','1998-12-08',24,8910208370,'Female'),
+('dsharma_2207','Divya Sharma','P2 Awadh Puri Opposite Ram Mandir Lalkothi Jaipur Rajasthan 302001','1970-07-22',53,8433284469,'Female'),
+('ragarwal_0310','Ritu Agarwal','Block-c 24 Bangur Avenue Bangur Kolkata West Bengal700055','1992-10-03',31,7828008746,'Female'),
+('hmenon_1804','Harish Menon','4-49/2 Saroornagar Hyderabad 500035','1990-04-18',33,7478290365,'Male'),
+('asrivastava_0809','Anjali Srivastava','Surycity Takrohi Road Indira Nagar Lucknow Uttar Pradesh 227105','1974-09-08',49,8369108575,'Female'),
+('rmenon_1104','Radha Menon','15 Patia GIDC Vatva Ahmedabad Gujarat 382445','1997-04-11',26,7783970427,'Female'),
+('gdesai_1812','Geeta Desai','Dayal Bagh Rd Swamibagh DayalBagh Agra Uttar Pradesh 282005','2000-12-18',22,8306851931,'Female'),
+('vagarwal_2208','Vinay Agarwal', '1st Floor Haware Centurion Mall Sector 19/A Nerul Near Seawood Station Nerul Navi Mumbai 400706','1973-08-22',50,7823671652,'Male'),
+('akhanna_1509','Alok Khanna','Kashmir Ave Rd Kashmir Avenue Amritsar Punjab 143001','1986-09-15',37,8966027097,'Male'),
+('vthakur_0211','Vikram Thakur','24 Tilak Marg New delhi 020626','1991-11-02',31,7347751353,'Male'),
+('rsharma_2803','Rajat Sharma','Awantipur Srinagar Jammu and Kashmir 190010','1983-03-28',40,7086532187,'Male'),
+('ngupta_3006','Nikhil Gupta','7 Pragjyotish Nagar Maligaon Guwahati Assam 781033','2002-06-03',21,8666589183,'Male'),
+('smehta_1506','Suresh Mehta','Plexus ITI Rd Aundh Pune 411007','1971-06-15',52,8907756410,'Male'),
+('rdesai_1212','Rajesh Desai','326 Gali Sangt Trashan Dariba Kalan Chandni Chowk New Delhi Delhi 110006','1995-12-12',27,7839323773,'Male'),
+('stwari_2608','Swati Tiwari','Dayanandnagar Nigamananda Nagar Rasulgarh Bhubaneswar Odisha 751007','1993-08-26',30,7214777014,'Female'),
+('dmalhotra_2509','Deepak Malhotra','2nd mile Sevoke Rd Siliguri West Bengal 734001','1984-09-25',39,8203231719,'Male'),
+('ryadav_1604','Ramesh Yadav',"1286 Opposite St Michael's Church Kumar Vaddo Anjuna Goa 403509",'1991-04-16',32,8205496565,'Male'),
+('amehta_2303','Aarti Mehta','G2 EVEREST APT 31 Saket Nagar Indore Madhya Pradesh 452010','1988-02-23',25,8806959952,'Female'),
+('njoshi_1411','Neha Joshi','K110 Basement Hauz Khas Enclave New Delhi Delhi 110016','1987-11-14',35,8404216528,'Female'),
+('areddy_0605','Aishwarya Reddy','Mohaton Ka Chauk Bikaner Rajasthan 334001','1972-05-06',51,7098571027,'Female');
+
+
+select * from user;
+
+
+use railway;
+
+drop table if exists Class;
+
+create table Class(
+Train_No int(50) NOT NULL Primary Key,
+Class_type varchar(255) NOT NULL,
+Fare_Rs int(50) NOT NULL
+);
+
+
+
+INSERT INTO Class VALUES
+(12305,'3A',2380),
+(19715,'3E',990),
+(12315,'SL',810),
+(17649,'2S',190),
+(12229,'GEN',180),
+(19031,'2A',23605),
+(19817,'SL',365),
+(12240,'1A',5550),
+(12498,'CC',170),
+(12497,'CC',150),
+(19224,'3A',1590),
+(15654,'SL',2200),
+(12298,'2A',1610),
+(15127,'2S' ,1575),
+(22881,'2A',2675),
+(15909,'GEN',525),
+(12779,'GEN',455),
+(19326,'1A',3730),
+(22985,'SL',455),
+(20845,'1A',4865);
+
+
+select * from class;
+
+use railway;
+
+drop table if exists ticket;
+
+
+create table Ticket(
+Username varchar(255) NOT NULL,
+PNR_NO bigint Primary Key NOT NULL,
+Train_No int(10) NOT NULL,
+Train_Name varchar(255) NOT NULL,
+Date_Time Datetime NOT NULL,
+Passenger_name varchar(255) NOT NULL,
+Class varchar(225) NOT NULL,
+Source varchar(255) NOT NULL,
+Destination varchar(255) NOT NULL,
+Date_of_Boarding date,
+Coach_no varchar(255) NOT NULL,
+Seat_no int(10) NOT NULL
+);
+
+Insert into Ticket values
+('rsharma_2803',5526186862,19224,'Jammu Tawi Ahmedabad Express','2023-01-04 07:50','Rajat Sharma','3A','Jammu Tawi [JAT]','Ahmedabad Junction [ADI]','2023-05-29','B2',19),
+('dmalhotra_2509',8401432009,15909,'Avadh Assam Express','2023-01-20 10:20','Deepak Malhotra','GEN','Dibrugarh [DBRG]','Lalgarh Junction [LGH]','2023-06-10','GS',42),
+('njoshi_1411',9435901103,22985,'Rajasthan Humsafar Express','2023-01-22 12:45','Neha Joshi','SL','Udaipur City [UDZ]','Delhi Sarai Rohilla [DEE]','2023-06-12','S3',77),
+('akhanna_1509',4512865253,12498,'Shan E punjab Express','2023-02-03 15:10','Alok Khanna','CC','Amritsar Junction [ASR]','New Delhi [NDLS]','2023-06-27','C1',56),
+('areddy_0605',9465877062,20845,'Bilaspur Bikaner Super Fast Express','2023-02-11 18:25','Aishwarya Reddy','1A','Bilaspur Junction [BSP]','Bikaner Junction [BKN]','2023-07-02','H1',43),
+('stwari_2608',7349494305,22881,'Pune Bhubaneswar Superfast Express','2023-03-02 11:15','Swati Tiwari','2A','Pune Junction [PUNE]','Bhubaneswar [BBS]','2023-07-19','A3',99),
+('hmenon_1804',3650462331,17649,'Hyderabad Aurangabad Express','2023-03-10 22:45','Harish Menon','2S','Hyderabad Deccan [HYB]','Aurangabad [AWB]','2023-08-12','D3',48),
+('ragarwal_0310',3019128634,12315,'Ananya Express','2023-03-16 13:10','Ritu Agarwal','SL','Kolkata [KOAA]','Udaipur City [UDZ]','2023-08-23','S2',56),
+('ppatel_0812',1660252115,12305,'Howrah New Delhi Rajdhani Express','2023-03-19 14:05','Priya Patel','3A','Howrah Junction [HWH]','New Delhi [NDLS]','2023-08-25','B1',17),
+('smehta_1506',7104143169,12298,'Pune Ahmedabad AC Duronto Express','2023-03-27 21:35','Suresh Mehta','2A','Pune Junction [PUNE]','Ahmedabad Junction [ADI]','2023-08-28','A1',42),
+('ngupta_3006',6133609493,15654,'Jammu Tawi Guwahati Amarnath Express','2023-04-07 22:45','Nikhil Gupta','SL','Jammu Tawi [JAT]','Guwahati [GHY]','2023-09-17','S1',89),
+('vthakur_0211',5308484898,12497,'Shan E punjab Express','2023-04-18 06:40','Vikram Thakur','CC','New Delhi [NDLS]','Amritsar Junction [ASR]','2023-09-28','C3',25),
+('amehta_2303',9326306131,19326,'Amritsar Indore Express','2023-05-07 01:50','Aarti Mehta','1A','Amritsar Junction [ASR]','Indore Junction [INDB]','2023-09-29','H3',2),
+('dsharma_2207',1742404505,19715,'Jaipur Gomti Nagar Express','2023-05-21 21:05','Divya Sharma','3E','Jaipur Junction [JP]','Gomti Nagar [GTNR]','2023-10-10','E2',6),
+('rmenon_1104',3897468571,19031,'Yoga Express','2023-06-04 10:50','Radha Menon','2A','Ahmedabad Junction [ADI]','Yog Nagari Rishikesh [YNRK]','2023-10-15','A2',63),
+('ryadav_1604',8800558546,12779,'Goa Express','2023-06-23 15:00','Ramesh Yadav','1A','Vasco da Gama [VSG]','Hazrat Nizamuddin [NZM]','2023-10-30','H1',10),
+('rdesai_1212',7120257744,15127,'Kashi Vishwanath Express','2023-07-07 13:30','Rajesh Desai','2S','Banaras [BSBS]','New Delhi [NDLS]','2023-11-10','D2',39),
+('asrivastava_0809',3803178653,12229,'Lucknow Mail','2023-07-25 22:00','Anjali Srivastava','GEN','Lucknow Junction [LJN]','New Delhi [NDLS]','2023-11-26','GS',90),
+('vagarwal_2208',4259913581,12240,'Hisar Mumbai AC Duronto Express','2023-08-08 10:00','Vinay Agarwal','1A','Hisar Junction [HSR]','Mumbai Central [MMCT]','2023-12-11','H1',12),
+('gdesai_1812',4004823309,19817,'Haldighati Express','2023-08-14 08:45','Geeta Desai','SL','Ratlam Junction [RTM]','Agra Fort [AF]','2023-12-25','S2',84);
+
+
+
+select * from ticket;
+
+use railway;
+
+drop table if exists Train;
+CREATE TABLE Train (
+    TrainNumber INT PRIMARY KEY,
+    TrainName VARCHAR(255),
+    Source VARCHAR(255),
+    Destination VARCHAR(255),
+    ArrivalTime TIME,
+    DepartureTime TIME,
+    DistanceKms INT
+);
+
+
+INSERT INTO Train (TrainNumber, TrainName, Source, Destination, ArrivalTime, DepartureTime, DistanceKms)
+VALUES
+    (12305, 'Howrah New Delhi Rajdhani Express', 'Howrah Junction [HWH]', 'New Delhi [NDLS]', '14:05:00', '10:05:00', 1524),
+    (19715, 'Jaipur Gomti Nagar Express', 'Jaipur Junction [JP]', 'Gomti Nagar [GTNR]', '21:05:00', '11:40:00', 693),
+    (12315, 'Ananya Express', 'Kolkata [KOAA]', 'Udaipur City [UDZ]', '13:10:00', '12:25:00', 2127),
+    (17649, 'Hyderabad Aurangabad Express', 'Hyderabad Deccan [HYB]', 'Aurangabad [AWB]', '22:45:00', '13:20:00', 583),
+    (12229, 'Lucknow Mail', 'Lucknow Junction [LJN]', 'New Delhi [NDLS]', '22:00:00', '06:55:00', 492),
+    (19031, 'Yoga Express', 'Ahmedabad Junction [ADI]', 'Yog Nagari Rishikesh [YNRK]', '10:50:00', '12:25:00', 1225),
+    (19817, 'Haldighati Express', 'Ratlam Junction [RTM]', 'Agra Fort [AF]', '08:45:00', '04:40:00', 691),
+    (12240, 'Hisar Mumbai AC Duronto Express', 'Hisar Junction [HSR]', 'Mumbai Central [MMCT]', '10:00:00', '08:20:00', 1504),
+    (12498, 'Shan E Punjab Express', 'Amritsar Junction [ASR]', 'New Delhi [NDLS]', '15:10:00', '22:30:00', 447),
+    (12497, 'Shan E Punjab Express', 'New Delhi [NDLS]', 'Amritsar Junction [ASR]', '06:40:00', '14:15:00', 447),
+    (19224, 'Jammu Tawi Ahmedabad Express', 'Jammu Tawi [JAT]', 'Ahmedabad Junction [ADI]', '07:50:00', '13:40:00', 1476),
+    (15654, 'Jammu Tawi Guwahati Amarnath Express', 'Jammu Tawi [JAT]', 'Guwahati [GHY]', '22:45:00', '23:00:00', 2437),
+    (12298, 'Pune Ahmedabad AC Duronto Express', 'Pune Junction [PUNE]', 'Ahmedabad Junction [ADI]', '21:35:00', '06:25:00', 622),
+    (15127, 'Kashi Vishwanath Express', 'Banaras [BSBS]', 'New Delhi [NDLS]', '13:30:00', '05:40:00', 795),
+    (22881, 'Pune Bhubaneswar Superfast Express', 'Pune Junction [PUNE]', 'Bhubaneswar [BBS]', '11:15:00', '16:45:00', 1707),
+    (15909, 'Avadh Assam Express', 'Dibrugarh [DBRG]', 'Lalgarh Junction [LGH]', '10:20:00', '04:10:00', 3117),
+    (12779, 'Goa Express', 'Vasco da Gama [VSG]', 'Hazrat Nizamuddin [NZM]', '15:00:00', '06:25:00', 2159),
+    (19326, 'Amritsar Indore Express', 'Amritsar Junction [ASR]', 'Indore Junction [INDB]', '01:50:00', '12:55:00', 1343),
+    (22985, 'Rajasthan Humsafar Express', 'Udaipur City [UDZ]', 'Delhi Sarai Rohilla [DEE]', '12:45:00', '11:25:00', 667),
+    (20845, 'Bilaspur Bikaner Super Fast Express', 'Bilaspur Junction [BSP]', 'Bikaner Junction [BKN]', '18:25:00', '03:25:00', 1886);
+
+select * from train;
+
+alter table train
+change ArrivalTime Scheduled_Arrival Time;
+
+alter table train
+change DepartureTime Scheduled_Departure Time;
+
+use railway;
+
+drop table if exists Payments;
+
+
+CREATE TABLE Payments (
+	Username varchar(255) NOT NULL,
+    transaction_id VARCHAR(15),
+    account_number VARCHAR(11),
+    passenger_name VARCHAR(50),
+    bank_name VARCHAR(50),
+    amount DECIMAL(10, 2)
+);
+
+
+INSERT INTO payments (username,transaction_id, account_number, passenger_name, bank_name, amount)
+VALUES
+    ('ppatel_0812','202310250120306', '87123456789', 'Priya Patel', 'State Bank of India (SBI)', 2380.00),
+    ('dsharma_2207','172508061754913', '54321012345', 'Divya Sharma', 'HDFC Bank', 990.00),
+    ('ragarwal_0310','281711192123456', '98765431209', 'Ritu Agarwal', 'ICICI Bank', 810.00),
+    ('hmenon_1804','141805172543219', '34567898765', 'Harish Menon', 'Axis Bank', 190.00),
+    ('asrivastava_0809','290206011287634', '45678901238', 'Anjali Srivastava', 'ICICI Bank', 180.00),
+    ('rmenon_1104','161202221341298', '12340987652', 'Radha Menon', 'Punjab National Bank (PNB)', 2360.00),
+    ('gdesai_1812','251908191769420', '87654321097', 'Geeta Desai', 'Yes Bank', 365.00),
+    ('vagarwal_2208','192304022525456', '43210987651', 'Vinay Agarwal', 'State Bank of India (SBI)', 5550.00),
+    ('akhanna_1509','071607101234567', '98765432106', 'Alok Khanna', 'ICICI Bank', 170.00),
+    ('vthakur_0211','202211111224571', '21098765436', 'Vikram Thakur', 'State Bank of India (SBI)', 150.00),
+    ('rsharma_2803','220919301234567', '98765432105', 'Rajat Sharma', 'ICICI Bank', 1590.00),
+    ('ngupta_3006','080604281923456', '87654321984', 'Nikhil Gupta', 'Bank of Baroda', 2200.00),
+    ('smehta_1506','051501161719283', '23456789013', 'Suresh Mehta', 'State Bank of India (SBI)', 1610.00),
+    ('rdesai_1212','130706071234592', '56781234567', 'Rajesh Desai', 'Yes Bank', 1575.00),
+    ('stwari_2608','031607191234508', '45678902145', 'Swati Tiwari', 'ICICI Bank', 2675.00),
+    ('dmalhotra_2509','200110051234567', '89012345677', 'Deepak Malhotra', 'Punjab National Bank (PNB)', 525.00),
+    ('ryadav_1604','260701091234567', '54321210987', 'Ramesh Yadav', 'Bank of Baroda', 455.00),
+    ('amehta_2303','191814010152436', '43210987214', 'Aarti Mehta', 'HDFC Bank', 3730.00),
+    ('njoshi_1411','121212121234567', '10987654328', 'Neha Joshi', 'Bank of Baroda', 455.00),
+    ('areddy_0605','170123456789012', '76543210987', 'Aishwarya Reddy', 'State Bank of India (SBI)', 4865.00);
+
+
+
+select * from payments;
+
+use railway;
+
+drop table if exists Routes;
+
+
+CREATE TABLE Routes (
+TrainNumber VARCHAR(255) Primary Key,
+Via VARCHAR(255),
+Max_Time_Halt TIME,
+Min_Time_Halt TIME,
+Distancekms INT
+);
+
+INSERT INTO Routes (TrainNumber, Via, Max_Time_Halt, Min_Time_Halt, Distancekms)
+VALUES
+(12305, 'HWH-PNBE-CNB-NDLS', '00:10:00', '00:02:00', 1531),
+(19715, 'JP-KL-MTJ-KJN', '00:10:00', '00:02:00', 522),
+(12315, 'KOAA-PNBE-AF-UDZ', '00:15:00', '00:02:00', 2131),
+(17649, 'HYB-BIDR-PRLI-AWB', '00:20:00', '00:01:00', 582),
+(12229, 'LJN-SPN-MB-NDLS', '00:08:00', '00:02:00', 493),
+(19031, 'ADI-ABR-DLI-YNRK', '00:15:00', '00:02:00', 1231),
+(19817, 'RTM-SRI-KOTA-AF', '00:25:00', '00:01:00', 696),
+(12240, 'HSR-JP-RTM-MMCT', '00:30:00', '00:02:00', 1511),
+(12498, 'ASR-PGW-UMB-NDLS', '00:10:00', '00:02:00', 448),
+(12497, 'NDLS-UMB-PGW-ASR', '00:06:00', '00:02:00', 448),
+(19224, 'JAT-BTI-JU-ADI', '00:25:00', '00:02:00', 1477),
+(15654, 'JAT-LKO-MFP-GHY', '00:10:00', '00:02:00', 2431),
+(12298, 'PUNE-LNL-ST-ADI', '00:05:00', '00:02:00', 635),
+(15127, 'BSBS-RBL-LKO-NDLS', '00:10:00', '00:02:00', 797),
+(22881, 'PUNE-SC-VSKP-BBS', '00:10:00', '00:01:00', 1702),
+(15909, 'DBRG-GHY-LKO-LGH', '00:25:00', '00:02:00', 3115),
+(12779, 'VSG-PUNE-VGLJ-NZM', '00:10:00', '00:02:00', 2208),
+(19326, 'ASR-UMB-AGC-INDB', '00:15:00', '00:02:00', 1337),
+(22985, 'UDZ-BHL-RE-DEE', '00:10:00','00:02:00',668),
+(20845,'BSP-BPL-KOTA-BKN','00:15:00','00:02:00',1892);
+
+
+select * from routes;
+
+ALTER TABLE Ticket
+ADD CONSTRAINT FK_Train_No
+FOREIGN KEY (Train_No)
+REFERENCES Train (TrainNumber);
+
+
+show create table Ticket;
+show full tables;
+
+SELECT t.Passenger_name,tr.TrainName
+FROM Ticket t
+JOIN Train tr ON t.Train_No = tr.TrainNumber;
+
+SELECT SUM(amount) AS TotalAmountSpent
+FROM Payments;
+
+SELECT Passenger_name,Class
+FROM Ticket
+WHERE Class = '2A';
+
+SELECT TrainNumber, Via
+FROM Routes
+WHERE TrainNumber IN (SELECT TrainNumber FROM Train WHERE Source = 'Jammu Tawi [JAT]');
+
+
+SELECT passenger_name, bank_name
+FROM Payments
+WHERE amount > 2000;
+
+SELECT Class_type, AVG(Fare_Rs) AS TotalFare
+FROM Class
+GROUP BY Class_type;
+
+
+SELECT PNR_NO, Passenger_name
+FROM Ticket
+WHERE Source = 'Howrah Junction [HWH]' AND Destination = 'New Delhi [NDLS]';
+
+
+SELECT TrainNumber, Source, Destination
+FROM Train
+WHERE DistanceKms > 1500;
+
+SELECT TrainName, Scheduled_Departure
+FROM Train
+WHERE Source = 'Howrah Junction [HWH]' AND DistanceKms > 1000;
+
+SELECT PNR_NO, Train_No, Train_Name, Passenger_name
+From Ticket
+Where Source = 'Pune Junction [PUNE]';
+
+
+
+SELECT *
+FROM Routes
+WHERE TrainNumber IN (
+SELECT TrainNumber
+FROM Train
+WHERE Source = 'Pune Junction [PUNE]'
+)
+LIMIT 2;
+
+SELECT AVG(Fare_Rs) AS AverageFare
+FROM Class
+WHERE Class_type = '3A';
+
+SELECT *
+FROM Payments
+WHERE bank_name LIKE '%ICICI%';
+
+
+SELECT Train_No
+FROM Class
+WHERE Fare_Rs = (SELECT MAX(Fare_Rs) FROM Class WHERE Class_type = '1A');
+
+
+SELECT Passenger_name
+FROM Ticket
+WHERE Seat_no < 30;
+
+
+
+ALTER TABLE Ticket
+ADD CONSTRAINT FK_user
+FOREIGN KEY (Username) REFERENCES User(Username);
+
+desc train;
+
+ALTER TABLE Ticket
+ADD CONSTRAINT FK_trainno
+FOREIGN KEY (Train_No) REFERENCES Train(TrainNumber);
+
+desc Train;
+
+Alter table Class
+modify Train_No int Not Null Primary Key;
+
+desc Class;
+
+ALTER TABLE Class
+ADD CONSTRAINT FK_class
+FOREIGN KEY (Train_No) REFERENCES Train(TrainNumber);
+
+ALTER TABLE Routes
+ADD CONSTRAINT FK_trainroutes
+FOREIGN KEY (TrainNumber) REFERENCES Train(TrainNumber);
+
+ALTER TABLE Payments
+ADD CONSTRAINT FK_username
+FOREIGN KEY (Username) REFERENCES User(Username);
